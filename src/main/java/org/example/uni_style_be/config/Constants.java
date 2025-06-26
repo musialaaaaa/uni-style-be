a661bee292295c176be4fd6a343ed125a3a420e3
musialaaaaa <liamdeii2005@gmail.com>
2025-06-26 22:09:50 +0700

    Revert "feature coupon"
    This reverts commit 00d27549

--- src/main/java/org/example/uni_style_be/config/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.example.uni_style_be.config;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class Constants {
-    public static final String SYSTEM = "SYSTEM";
-}
--- src/main/java/org/example/uni_style_be/model/filter/CategoryParam.java
+++
@@ -1,9 +0,0 @@
-package org.example.uni_style_be.model.filter;
-
-import lombok.Data;
-
-@Data
-public class CategoryParam {
-
-    private String name;
-}
